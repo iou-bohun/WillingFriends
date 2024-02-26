924e810095c56a772e92273fa86a91460abe4643
ruiroll <hyunbean67@gmail.com>
2024-02-26 20:29:39 +0900

    Feat - Character Jump
    점프 기능 추가

---
+++ Assets/Scripts/Character/CharacterJump.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterJump : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private float jumpForce = 2f;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
