d5e0a85fc872d1606f891db68c636255fa005f67
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-02-26 19:30:08 +0900

    Fix - PlatformGenerator 로직 수정
    랜덤생성 제거 및 플레이어 이동 거리에 따라 생성 여부 결정

--- Assets/GJYFolder/Scripts/Platform/PlatformGenerator.cs
+++ Assets/GJYFolder/Scripts/Platform/PlatformGenerator.cs
@@ -7,60 +7,101 @@ using Random = UnityEngine.Random;
 
 public class PlatformGenerator : MonoBehaviour
 {
-    // To Do - 딕셔너리로 바꾸기 (풀링 & Resources랑 연계)
-    // private Dictionary<Type, Platform[]> _platforms = new Dictionary<Type, Platform[]>();
+    public Action<int> OnGeneratePlatform; // 굳이 액션으로 할 필요가...? 있을거 같음 ㅇㅇ;; 플레이어가 앞으로 가는 Vector3 z값을 받아와야됨
 
-    [Header("# Collection_Temp")]
-    public List<GameObject> _prefabs;
-    public List<Platform> _platforms;
+    public List<GameObject> _prefabs = new List<GameObject>();
+
+    private Queue<GameObject> _platformsQueue = new Queue<GameObject>();    
 
     [Header("# Init")]
-    [Range(0, 10)] public int _startPositionZ;
+    [Range(-10, 10)] public int _startPositionZ;
 
     [Header("# Test")]
-    [Range(0, 10)] public int _testInitPlatformsCount;
-    
+    [Range(0, 20)]
+    public int _initPlatformsCount;
+    public int _autoDisableIndex = 20;
 
     private Vector3 _lastPlatformPos;
     private WaitForSeconds _waitTime = new WaitForSeconds(1);
+    
+    private int _currentStep = 0;
 
     [Header("# CustomEditor")]
     public bool _isGenerating = false;
 
     private void Awake()
     {
-        _lastPlatformPos = Vector3.forward * _startPositionZ;        
+        _lastPlatformPos = Vector3.forward * _startPositionZ;
 
-        for (int i = 0; i < _testInitPlatformsCount; i++)
+        for (int i = 0; i < _initPlatformsCount; i++)
         {
-            int randomTemp = Random.Range(0, _prefabs.Count);
-            _platforms.Add(GenerateRandomPlatform(randomTemp));
+            // To Do - 생성할 Type 넘겨주기
+            PlatformType type = PlatformType.Land;
+
+            GenerateRandomPlatform(type);
         }
-        
-        StartCoroutine(AutoGenerate_Test());
     }
 
-    public Platform GenerateRandomPlatform(int index)
+    // Temp
+    public GameObject testPlayer;
+
+    private void Update()
     {
-        GameObject clone = Instantiate(_prefabs[index], transform);
+        // To Do - 앞으로 갈 때 인덱스가 추가, 인덱스가 추가 될 때마다 현재 생성된 개수와 검사해서 생성 혹은 무시
 
-        Platform platform = clone.GetComponent<Platform>();
-        platform.transform.position = _lastPlatformPos + Vector3.forward;
-        _lastPlatformPos = platform.transform.position;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            testPlayer.transform.position += Vector3.forward;            
 
-        return platform;
+            if (_currentStep > _autoDisableIndex)
+            {
+                DisableOldestPlatform();
+                return;
+            }                
+
+            _currentStep++;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            testPlayer.transform.position += Vector3.back;
+            _currentStep--;
+            Debug.Log($"뒤로 : 스텝 {_currentStep}");
+            Debug.Log($"Queue Count{_platformsQueue.Count}");
+        }
     }
-
-    public Platform GenerateRandomPlatform(PlatformType platformType)
+    
+    public void GenerateRandomPlatform(PlatformType platformType)
     {
-        GameObject clone = Instantiate(_prefabs[(int)platformType], transform);
+        GameObject go = Instantiate(_prefabs[(int)platformType], transform);
+        go.transform.position = _lastPlatformPos;
+        _lastPlatformPos += Vector3.forward;
 
-        Platform platform = clone.GetComponent<Platform>();
-        platform.transform.position = _lastPlatformPos + Vector3.forward;
+        if (go.TryGetComponent(out PlatformBase platform) == false)
+        {
+            Debug.Log("PlatformBase 컴포넌트가 없습니다.");
+            return;
+        }
 
-        return platform;
+        _platformsQueue.Enqueue(go);
+        platform.Init();        
     }
 
+    private void DisableOldestPlatform()
+    {
+        // To Do - 파괴를 풀링으로 교체
+        if (_platformsQueue.Count == 0)
+        {
+            Debug.Log("플랫폼 Queue 개수 0 개... 그럴리가?");
+            return;
+        }
+
+        GameObject go = _platformsQueue.Dequeue().gameObject;
+        Destroy(go);
+
+        GenerateRandomPlatform(PlatformType.Land);
+    }
+
+    #region Editor - Inspector Custom Button Methods
     private IEnumerator AutoGenerate_Test()
     {
         _isGenerating = true;
@@ -68,10 +109,9 @@ public class PlatformGenerator : MonoBehaviour
         while (true)
         {
             yield return _waitTime;
-
-            int randomTemp = Random.Range(0, _prefabs.Count);
-            _platforms.Add(GenerateRandomPlatform(randomTemp));
-        }        
+            
+            GenerateRandomPlatform(PlatformType.Land);
+        }
     }
 
     public void StartGenerate()
@@ -80,7 +120,7 @@ public class PlatformGenerator : MonoBehaviour
         {
             Debug.Log("Already being Generated");
             return;
-        }            
+        }
 
         StartCoroutine(AutoGenerate_Test());
     }
@@ -90,4 +130,5 @@ public class PlatformGenerator : MonoBehaviour
         StopAllCoroutines();
         _isGenerating = false;
     }
+    #endregion
 }
