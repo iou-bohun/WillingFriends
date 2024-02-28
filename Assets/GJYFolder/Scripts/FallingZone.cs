a725bb123b178af7250087914c9506aa8a8d1964
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-02-29 01:47:22 +0900

    Feat - Player 물에 빠짐 로직 추가

---
+++ Assets/GJYFolder/Scripts/FallingZone.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingZone : MonoBehaviour
+{
+    [SerializeField] ParticleSystem _waterEffect;    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 게임오버 로직은 GameManager 총괄.
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("게임오버");
+            GameObject effect = Instantiate(_waterEffect.gameObject);
+            effect.transform.position = other.transform.position;
+
+            StartCoroutine(Co_ReturnParticle(effect));
+        }            
+    }
+
+    private IEnumerator Co_ReturnParticle(GameObject effect)
+    {
+        yield return new WaitForSeconds(2f);
+
+        ObjectPoolManager.ReturnObject(effect.name, effect);
+    }
+}
