40c0692504471b0387a69e1af6d53c1db7cd974c
정원우 <54103715+klik075@users.noreply.github.com>
2024-02-28 20:44:17 +0900

    Feat - 메인 씬에 사운드매니저 추가, start 트리거 추가

--- Assets/JWWFolder/Scripts/StartTrigger.cs
+++ Assets/JWWFolder/Scripts/StartTrigger.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class StartTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         Managers.Instance.CallInitEvent();
         //GameManager.Instance.Initialize();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(gameObject);
     }
 }
